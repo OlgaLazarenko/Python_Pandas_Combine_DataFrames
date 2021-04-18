31ae1653d86737eeb518999a7b3552de5c0c68cd
Olga Lazarenko <olazarenko@outlook.com>
2021-04-18 00:00:40 -0400

    read next imported auto dataset

--- Combine DataFrames.py
+++ Combine DataFrames.py
@@ -11,3 +11,5 @@ print('Here I will combine dataframes')
 auto_df1 = pd.read_csv("E:\_Python_Projects_Data\Combine_DataSets\Imported_Autos\Auto_DataSet_1.csv")
 print(auto_df1)
 
+auto_df2 = pd.read_csv("E:\_Python_Projects_Data\Combine_DataSets\Imported_Autos\Auto_DataSet_2.csv")
+print(auto_df2)
