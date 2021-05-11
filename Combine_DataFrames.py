b35a3bb437a0f6d2db4010cc15fc8a971fbe92de
Olga Lazarenko <olazarenko@outlook.com>
2021-05-10 23:52:35 -0400

    create subsets of the data frame <<auto_df> / Combine_DataFrames

--- Combine_DataFrames.py
+++ Combine_DataFrames.py
@@ -165,45 +165,33 @@ print()
 print()
 
 print('------------------------------------------------------------------------------')
-# create <auto> DataFrame by reading the data file
-auto = pd.read_csv("E:\_Python_Projects_Data\Data_Visualization\Autos_Data_Set\Autos_Import_1985.csv" , 
-                  usecols = ['Make' ,
-                              'Body Style' , 
-                              'City mpg' ,
-                              'Highway mpg',  
-                              'Price'])
-print('<auto> DataFrame')
-print(auto)
+print()
+print('Create subset of the DataFrame <auto>')
 
-# insert a new column <CarID> at the DataFrame <auto>
-print()
-column_name = "CarID"
-first_column = auto_df.pop(column_name)
-print(auto.head())
-print()
-# use the function insert() to insert the columns saved in the variable <first_column>
-auto.insert(0,"CarID",first_column) 
-print('auto with CarID at the first position')
-print(auto)
+auto_bmw = auto_df[auto_df['Make'] == 'bmw']
+print("auto_bmw ")
+print(auto_bmw)
 print()
 
-# remove some columns from <auto> DataFrame
-del auto['Price'] # delete the column 'Price'
-auto.pop('Body Style') # remove/pop the column 'Body Style' from the DataFrame <auto>
-auto.drop(['City mpg', 'Highway mpg'] , inplace = True , axis = 1 )
 
-# print create <country> DataFrame by reading the data file
-country = pd.read_csv("E:\_Python_Projects_Data\Data_Visualization\Autos_Data_Set\Car_Country.csv")
-print('<country> DataFrame')
-print(country)
+auto_audi = auto_df[auto_df["Make"] == 'audi']
+print('auto_audi')
+print(auto_audi)
 print()
-# OR 
-print(list(country.columns))
+
+auto_volvo = auto_df[auto_df["Make"] == 'volvo']
+print('auto_volvo')
+print(auto_volvo)
+print()
 
 '''
 # show the columns name
 for col in auto.columns:
    print(col)
+
+print()
+# OR 
+print(list(country.columns))
 '''
 
 
--- Join_DataFrames.py
+++ Join_DataFrames.py
@@ -150,5 +150,5 @@ print('auto_20')
 print(auto_20)
 
 # inner join
-auto_10_20_inner_join = pd.merge(auto_10 , auto_20 , how = 'inner' , on = 'CarID')
+auto_10_20_inner_join = pd.merge(auto_10_top , auto_20 , how = 'inner' , on = 'CarID')
 print(auto_10_20_inner_join)
